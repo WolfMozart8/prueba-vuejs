486a723f2e004055565093b66c07277ea94799b0
Felipe <115050091+WolfMozart8@users.noreply.github.com>
2025-06-03 15:50:41 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+    </div>
+  </header>
+
+  <main>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
